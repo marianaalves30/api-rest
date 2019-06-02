f6e18cec163ff72b9699b519fda631e8b4fa9f32
Mariana Alves <mahhry@gmail.com>
2019-06-02 13:31:00 -0300

    curso API REST com Spring

---
+++ src/main/java/br/com/xyinc/xyinc/XyIncApplication.java
@@ -0,0 +1,13 @@
+package br.com.xyinc.xyinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyIncApplication.class, args);
+	}
+
+}
