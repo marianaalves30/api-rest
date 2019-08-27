9bf5e877b7ac3024cf346635e489dfb2956566d0
Mariana Alves <mahhry@gmail.com>
2019-08-27 17:35:37 -0300

    27-8

---
+++ src/main/java/br/com/xyinc/xyinc/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.xyinc.xyinc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.xyinc.xyinc.models.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+}
